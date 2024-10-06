ba93d60de1f036a73ec1e90855b94d160d1ffea9
Doan01062002 <vandoan01062002@gmail.com>
2024-10-06 21:19:49 +0700

    session15

---
+++ src/components/LyThuyet.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- Vue router
+      1. Tổng quan về vue-router
+        - là một thư viện dành riêng cho vuejs dành riêng cho các trang (cpmponent) quản lý các routes mà không làm tải lại trang
+  
+      2. cấu hình
+        - Cách 1: khi tạo dự án vue chọn option vue-router
+        - Cách 2: Tạo dự án bình thường ==> cài đặt vue-router
+      3. Navigation(điều hướng)
+  
+      4. dynamic router(id...)
+  
+      5. nested router 
+  
+      -->
+  </div>
+</template>
+<script setup></script>
+<style></style>
